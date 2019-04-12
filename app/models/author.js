f9aac9a452f57062ec6813fdd3d9321339084a35
Alicia Hetrick <ahetrick22@gmail.com>
2019-04-12 09:14:47 -0400

    show author biography from db in template instead of hard-coded bio

--- app/models/author.js
+++ app/models/author.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Publisher from './publisher';
 import { hasMany } from 'ember-data/relationships';
 
 export default Publisher.extend({
+  biography: DS.attr('string'),
   books: hasMany('book', { async: true }),
 
   loadedAt: Ember.on('didLoad', function() {
--- app/routes/books.js
+++ app/routes/books.js
@@ -20,4 +20,4 @@ export default Ember.Route.extend({
     }
 
   }
-})
+});
