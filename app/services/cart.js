00798f55757868426b12a5e00b0756124334fb07
Alicia Hetrick <ahetrick22@gmail.com>
2019-04-19 12:38:08 -0400

    moved logic into cart service, created new component to list all items in cart

--- app/services/cart.js
+++ app/services/cart.js
@@ -1,20 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  addToCart(title, author, price) {
-    //get the existing cart and add the book, or if there's not a cart then create a new cart with the book in it
-    let cart = JSON.parse(localStorage.getItem('cart'));
-    if (!cart) {
-      cart = [];
-    } 
-    cart.push({ title, author, price });
-    localStorage.setItem('cart', JSON.stringify(cart));
 
+  init() {
+  // get the cart our of local storage (this way it persists in the user's browser past a reload)
+  this.set('cart', JSON.parse(localStorage.getItem("cart")));
+  if (!this.cart) { 
+    this.set('cart',[]); 
+  }
+  // send the current total in the cart to the template as well
+  this.set('total',this._calcTotal());
   },
 
-  emptyCart() {
-    localStorage.removeItem('cart');
-    window.location.reload();
-  }
+  //add an item to the cart in both local storage and the current state
+  addToCart(title, author, price) {
+    this.cart.pushObject({ title, author, price });
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+    this.set('total', this._calcTotal());
+  },
 
+  //clear out the cart in local storage and in state
+  emptyCart() {
+    localStorage.setItem('cart', '[]');
+    this.set('cart',[]);
+    this.set('total', 0);
+  },
+
+  //internally find the current total of prices in the cart
+  _calcTotal() {
+    return this.cart.reduce((accumulator, current)  => {
+      return accumulator + current.price;
+    },0);
+  }
 });
